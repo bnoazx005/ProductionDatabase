247998f4f0eb2142bbcaab2a7d0d75e5e1a7f276
jekyll0_0 <ildar2571@yandex.ru>
2017-09-29 15:14:51 +0400

    Add a new code for work with the database
    Changes:
    * A new piece of code was added to store information about materials into
    the database.

--- Sources/src/production_database/CApplication.java
+++ Sources/src/production_database/CApplication.java
@@ -18,6 +18,13 @@ public final class CApplication {
 		try {
 			databaseInstance = Db4o.openFile("production.data");
 
+			_createIfNotExistsMaterial(databaseInstance, new CMaterial("Alluminium", "kg./rub.", 70, E_MATERIAL_TYPE.MT_ALUMINIUM));
+			_createIfNotExistsMaterial(databaseInstance, new CMaterial("Copper wire", "kg./rub.", 300, E_MATERIAL_TYPE.MT_COPPER));
+			_createIfNotExistsMaterial(databaseInstance, new CMaterial("Humbucker pickup magnet", "element/rub.", 420, E_MATERIAL_TYPE.MT_MAGNET));
+			_createIfNotExistsMaterial(databaseInstance, new CMaterial("Single pickup magnet", "element/rub.", 390, E_MATERIAL_TYPE.MT_MAGNET));
+			_createIfNotExistsMaterial(databaseInstance, new CMaterial("Ebony board", "40cmх40cmх1000cm/rub.", 2750, E_MATERIAL_TYPE.MT_EBONY));
+			_createIfNotExistsMaterial(databaseInstance, new CMaterial("Mahogany board", "m^3/rub.", 65000, E_MATERIAL_TYPE.MT_MAHOGANY));
+			
 			_createIfNotExistsManufacturer(databaseInstance, new CCompany("Fokin Pickups", "315774600010405", "г. Москва, ул. Грина, д. 32"));
 			_createIfNotExistsManufacturer(databaseInstance, new CCompany("Lepsky Guitars", "758874643420489", "г. Краснодар ул Трамвайная, 13", "+79182189877"));
 		}
@@ -45,6 +52,24 @@ public final class CApplication {
 
 		System.out.println("The company " + company + " was successfully added into the database");
 	}
+	
+	private static void _createIfNotExistsMaterial(ObjectContainer database, IMaterial material) throws IllegalArgumentException {
+		if (material == null) {
+			throw new IllegalArgumentException("An input parameter cannot equal to null");
+		}
+		
+		ObjectSet existingSameMaterials = _findMaterialByName(database, material.GetName());
+		
+		if (existingSameMaterials.size() > 1) {
+			System.out.println("The material " + material + " is already exists in the database");
+			
+			return;
+		}
+		
+		database.store(material);
+
+		System.out.println("The company " + material + " was successfully added into the database");
+	}
 
 	private static ObjectSet _findCompanyByName(ObjectContainer database, String name) throws IllegalArgumentException {
 		if (name == null) {
@@ -59,5 +84,20 @@ public final class CApplication {
 		});
 		
 		return companiesSet;
+	}
+	
+	private static ObjectSet _findMaterialByName(ObjectContainer database, String name) throws IllegalArgumentException {
+		if (name == null) {
+			throw new IllegalArgumentException("A name parameter cannot equal to null");			
+		}
+		
+		ObjectSet materialsSet = database.query(new Predicate<IMaterial>() {
+			public boolean match(IMaterial material) {
+				//search for a match by a material's name
+				return material.GetName().equals(name);
+			}
+		});
+		
+		return materialsSet;
 	}
 }

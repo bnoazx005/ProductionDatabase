f9c4089d5cf8d48b9793add65390203f0ac6a561
jekyll0_0 <ildar2571@yandex.ru>
2017-09-29 10:41:50 +0400

    Add a description of a product
    Changes:
    * A new interface IProduct was added.
    * An implementation of IProduct, which is called CProduct, was added into
    production_database package of the project.

---
+++ Sources/src/production_database/CProduct.java
@@ -0,0 +1,177 @@
+/**
+ * CProduct.java
+ * 
+ * @author Kasimov Ildar
+ */
+
+package production_database;
+
+import production_database.interfaces.*;
+
+
+/**
+ * class CProduct
+ * 
+ * The class represents a product and implements IProduct interface.
+ * 
+ * @author Kasimov Ildar
+ */
+
+public final class CProduct implements IProduct {
+	private String  mName;
+	
+	private String  mCode;
+	
+	private boolean mIsStandard;
+	
+	private float   mAmountPerYear;
+	
+	private String  mAdditionalNote;
+	
+	/**
+	 * The main constructor 
+	 * @param name A name of a product
+	 * @param code A code of a product
+	 * @param isStandard Is a product standard
+	 * @param amountPerYear An amount of product's production per year
+	 * @param note An additional notes about product
+	 */
+	
+	public CProduct(String name, String code, boolean isStandard, float amountPerYear, String note) {
+		mName = name;
+		mCode = code;
+		
+		mIsStandard = isStandard;
+		
+		mAmountPerYear = amountPerYear;
+		
+		mAdditionalNote = note;
+	}
+	
+	/**
+	 * The simplified constructor with two parameters
+	 * 
+	 * It provides to sets up a name and a code of a product. By default mIsStandard field will set up to false,
+	 * and mAmountPerYear will be equal to 0.0f
+	 * @param name A name of a product
+	 * @param code A code of a product
+	 */
+	
+	public CProduct(String name, String code) {
+		mName = name;
+		mCode = code;
+		
+		mIsStandard = false;
+		
+		mAmountPerYear = 0.0f;
+	}
+	
+	/**
+	 * Default constructor is not used
+	 */
+	
+	@SuppressWarnings("unused")
+	private CProduct() {		
+	}
+
+	/**
+	 * The method sets up a name of a product
+	 * @param name A name of a product
+	 */
+	
+	@Override 
+	public void SetName(String name) {
+		mName = name;		
+	}
+	
+	/**
+	 * The method sets up a code of a product
+	 * @param code A string that contains code's value
+	 */
+	
+	@Override 
+	public void SetCode(String code) {
+		mCode = code;
+	}
+	
+	/**
+	 * The method specifies is a product standard or not
+	 * @param value A type of a product (true is a standard one, false is not)
+	 */
+	
+	@Override 
+	public void IsStandard(boolean value) {
+		mIsStandard = value;
+	}
+		
+	/**
+	 * The method sets up a production amount per year
+	 * @param amountPerYear A positive floating-point value that describes a production
+	 *  amount of a product per year
+	 */
+	
+	@Override 
+	public void SetAmountPerYear(float amountPerYear) {
+		mAmountPerYear = amountPerYear;
+	}
+	
+	/**
+	 * The method sets up an additional notes for a product
+	 * @param note A string with a comment or special notes about a product
+	 */
+	
+	@Override 
+	public void SetNote(String note) {
+		mAdditionalNote = note;
+	}
+	
+		
+	/**
+	 * The method returns a name of a product
+	 * @return A string with a name of a product
+	 */
+	
+	@Override 
+	public String GetName() {
+		return mName;
+	}
+	
+	/**
+	 * The method returns a code of a product
+	 * @return A string with a product's code
+	 */
+	
+	@Override 
+	public String GetCode() {
+		return mCode;
+	}
+	
+	/**
+	 * The method returns a type of a product
+	 * @return The method returns a true if a product is standard and false in other case
+	 */
+	
+	@Override 
+	public boolean IsStandard() {
+		return mIsStandard;
+	}
+	/**
+	 * The method returns a value of production amount per year
+	 * @return a value of product's production amount per year
+	 */
+	
+	@Override 
+	public float GetAmountPerYear() {
+		return mAmountPerYear;
+	}
+			
+	/**
+	 * The method returns a string with additional notes
+	 * @return A string with product's notes
+	 */
+	
+	@Override 
+	public String GetNote() {
+		return mAdditionalNote;
+	}
+}
---
+++ Sources/src/production_database/interfaces/IProduct.java
@@ -0,0 +1,86 @@
+package production_database.interfaces;
+
+
+
+/**
+ * Interface IProduct
+ * 
+ * The interface describes a single product's functionality
+ * 
+ * @author Kasimov Ildar
+ */
+
+public interface IProduct {
+	
+	/**
+	 * The method sets up a name of a product
+	 * @param name A name of a product
+	 */
+	
+	void SetName(String name);
+	
+	/**
+	 * The method sets up a code of a product
+	 * @param code A string that contains code's value
+	 */
+	
+	void SetCode(String code);
+	
+	/**
+	 * The method specifies is a product standard or not
+	 * @param value A type of a product (true is a standard one, false is not)
+	 */
+	
+	void IsStandard(boolean value);
+		
+	/**
+	 * The method sets up a production amount per year
+	 * @param amountPerYear A positive floating-point value that describes a production
+	 *  amount of a product per year
+	 */
+	
+	void SetAmountPerYear(float amountPerYear);
+	
+	/**
+	 * The method sets up an additional notes for a product
+	 * @param note A string with a comment or special notes about a product
+	 */
+	
+	void SetNote(String note);
+	
+		
+	/**
+	 * The method returns a name of a product
+	 * @return A string with a name of a product
+	 */
+	
+	String GetName();
+	
+	/**
+	 * The method returns a code of a product
+	 * @return A string with a product's code
+	 */
+	
+	String GetCode();
+	
+	/**
+	 * The method returns a type of a product
+	 * @return The method returns a true if a product is standard and false in other case
+	 */
+	
+	boolean IsStandard();
+		
+	/**
+	 * The method returns a value of production amount per year
+	 * @return a value of product's production amount per year
+	 */
+	
+	float GetAmountPerYear();
+			
+	/**
+	 * The method returns a string with additional notes
+	 * @return A string with product's notes
+	 */
+	
+	String GetNote();
+}
